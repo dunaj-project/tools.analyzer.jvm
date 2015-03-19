f3c23b0f4bab6efbe385bef866fbe30a609c74c8
Jozef Wagner <jozef.wagner@gmail.com>
2015-03-19 10:28:30 +0100

    deps bump

--- project.clj
+++ project.clj
@@ -11,5 +11,5 @@
   :dependencies [#_[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/core.memoize "0.5.6"]
                  [org.clojure/tools.reader "0.8.15"]
-                 [org.dunaj/tools.analyzer "0.6.6-dunaj_pre2"]
+                 [org.dunaj/tools.analyzer "0.6.6-dunaj_pre3"]
                  [org.ow2.asm/asm-all "4.2"]])
